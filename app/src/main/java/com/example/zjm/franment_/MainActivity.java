1018edf15e6e0cb05b66c06d2588ab1b1758f4d0
zjm <zjmwlx@126.com>
2017-05-14 21:44:19 +0800

    第一次上传

---
+++ app/src/main/java/com/example/zjm/franment_/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.zjm.franment_;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
