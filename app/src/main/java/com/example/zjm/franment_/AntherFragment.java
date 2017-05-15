0795e8a5e79266809751f319a1b044f993400894
zjm <zjmwlx@126.com>
2017-05-15 21:37:05 +0800

    第二次上传

---
+++ app/src/main/java/com/example/zjm/franment_/AntherFragment.java
@@ -0,0 +1,21 @@
+package com.example.zjm.franment_;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by zjm on 2017/5/15.
+ */
+
+public class AntherFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.anther_fagment, container, false);
+        return view;
+    }
+}
